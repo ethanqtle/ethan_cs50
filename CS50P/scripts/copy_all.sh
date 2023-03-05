42b7642880bac8403c460d9fdf1353f24fa84b6d
ethanqtle <77242606+ethanqtle@users.noreply.github.com>
2023-03-05 14:20:53 -0800

    Passed PSET4 now

--- CS50P/scripts/copy_all.sh
+++ CS50P/scripts/copy_all.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
+if [[ "$1" != "" ]];
+then
+    comment=$1
+else
+    comment="Update from workspace"
+fi
+echo "comment == $comment"
 cd /workspaces/77242606
 my_git=../ethan_cs50/CS50P
 cp -fr scripts $my_git/.
@@ -6,6 +13,6 @@ cp -fr pset* $my_git/.
 
 pushd $my_git
 git add -A
-git commit -a -m "Update from workspace"
+git commit -a -m "${comment}"
 git push
 popd
